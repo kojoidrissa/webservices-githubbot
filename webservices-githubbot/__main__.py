b9183f1b5c6dcd11471feb371a1618757b0670b8
Kojo Idrissa <kojo.idrissa@gmail.com>
2018-05-10 11:32:06 -0400

    added ⚡️

--- webservices-githubbot/__main__.py
+++ webservices-githubbot/__main__.py
@@ -12,7 +12,7 @@ async def issue_opened_event(event, gh, *args, **kwargs):
     """ Whenever an issue is opened, greet the author and say thanks."""
     url = event.data["issue"]["comments_url"]
     author = event.data["issue"]["user"]["login"]
-    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
+    message = f"Thanks for the report @{author}! I will look into it ASAP! As fast as ⚡️ (I'm a bot)."
     await gh.post(url, data={"body": message})
 
 
